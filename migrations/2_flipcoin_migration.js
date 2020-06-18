4469542f8483afeb8507f6e9a916b336f959985a
rubyredmoon <61881016+rubyredmoon@users.noreply.github.com>
2020-06-18 06:15:51 +0200

    Update 2_flipcoin_migration.js

--- migrations/2_flipcoin_migration.js
+++ migrations/2_flipcoin_migration.js
@@ -2,7 +2,7 @@ const Flipcoin = artifacts.require("Coinflip");
 
 module.exports = function(deployer, network, accounts) {
   deployer.deploy(Flipcoin).then(function(instance){
-    instance.fundContract({value: web3.utils.toWei("0.5","ether"), from: accounts[0]}).then(function(){
+    instance.flip({value: web3.utils.toWei("0.5","ether"), from: accounts[0]}).then(function(){
       console.log("The contract successfully got funded with 0.5 ether by the contract owner " + accounts[0]);
       console.log("The contract address is " + Flipcoin.address);
     }).catch(function(err){
